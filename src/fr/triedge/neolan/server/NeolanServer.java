59842c70d10f7702f00364491fb365e24c23ab08
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 18:01:15 +0200

    Added management of args

--- src/fr/triedge/neolan/server/NeolanServer.java
+++ src/fr/triedge/neolan/server/NeolanServer.java
@@ -10,12 +10,19 @@ import com.sun.net.httpserver.HttpServer;
 public class NeolanServer {
 
 	public static void main(String[] args) throws IOException {
-		HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 2021), 0);
+		int port = 50000;
+		String ip = "localhost";
+		if (args.length >= 2){
+			ip = args[0];
+			port = Integer.parseInt(args[1]);
+		}
+		
+		HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
 		server.createContext("/neolan", new  NeolanHttpHandler());
 		ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
 		server.setExecutor(threadPoolExecutor);
 		server.start();
-		System.out.println("Server started");
+		System.out.println("Server started on "+ip+":"+port);
 		
 	}
 
